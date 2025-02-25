dc9ffe918def53b2468af66a1ced4e06dcfd0af9
yareyynn <yarensakarya03@hotmail.com>
2025-02-25 13:21:15 +0300

    piscine projects added

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysakarya <ysakarya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/04 21:51:41 by ysakarya          #+#    #+#             */
+/*   Updated: 2024/02/04 21:56:08 by ysakarya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	a;
+	unsigned int	x;
+
+	a = 0;
+	x = ft_strlen(src);
+	if (size != 0)
+	{
+		while (src[a] != '\0' && a < size - 1)
+		{
+			dest[a] = src[a];
+			a++;
+		}
+		dest[a] = '\0';
+	}
+	return (x);
+}
